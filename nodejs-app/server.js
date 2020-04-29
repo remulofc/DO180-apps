2a1e68ee2b88a681db5a9496e0e96166bd32a7d5
Student User <student@workstation.lab.example.com>
2020-04-29 21:06:19 +0000

    ok

--- nodejs-app/server.js
+++ nodejs-app/server.js
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-var createError = require('html-errors');
-=======
-var createError = require('http-errors');
->>>>>>> f816d86aa10540b9f337c9eacb40ab14874e88e0
-
+var createErro = require('http-errors'); 
 var express = require('express');
-app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n')
-});
+app = express();  
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
-});
+app.get('/', function (req, res) {   res.send('Hello World from pod: ' + process.env.HOSTNAME + '\n') });  
 
+app.listen(8080, function () {   console.log('Example app listening on port 8080!'); });
